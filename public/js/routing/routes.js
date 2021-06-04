b310de4e5795f23136258c0aa29f9a3e78941b41
Stanislav Malík <stanislav.malik@student.tuke.sk>
2021-06-04 20:16:43 +0200

    add templates handling

--- public/js/routing/routes.js
+++ public/js/routing/routes.js
@@ -1,8 +1,29 @@
+import {home, weddingDress, socialDress, first, contact} from "../templates/templates.js";
 
 export default [
     {
         hash: "home",
         target: "router-view",
-        getTemplate: (targetElm) => document.getElementById(targetElm).innerHTML = document.getElementById("template-home").innerHTML
-    }
+        getTemplate: (targetElm) => document.getElementById(targetElm).innerHTML = home(),
+    },
+    {
+        hash: "wedding",
+        target: "router-view",
+        getTemplate: (targetElm) => document.getElementById(targetElm).innerHTML = weddingDress(),
+    },
+    {
+        hash: "social",
+        target: "router-view",
+        getTemplate: (targetElm) => document.getElementById(targetElm).innerHTML = socialDress(),
+    },
+    {
+        hash: "first",
+        target: "router-view",
+        getTemplate: (targetElm) => document.getElementById(targetElm).innerHTML = first(),
+    },
+    {
+        hash: "contact",
+        target: "router-view",
+        getTemplate: (targetElm) => document.getElementById(targetElm).innerHTML = contact(),
+    },
 ];
---
+++ public/js/templates/templates.js
@@ -0,0 +1,19 @@
+export const home = () => {
+    return `<p>hello from wedding dresses</p>`;
+}
+
+export const weddingDress = () => {
+    return `<p>hello from wedding dresses</p>`;
+}
+
+export const socialDress = () => {
+    return `<p>hello from social dresses</p>`;
+}
+
+export const first = () => {
+    return `<p>hello from 1.sv. dresses</p>`;
+}
+
+export const contact = () => {
+    return `<p>hello from contact</p>`;
+}
