6b10cdeaba608d1611c52c2782c7aac48df1040a
Stanislav Malík <stanislav.malik@student.tuke.sk>
2021-06-26 11:27:39 +0200

    update

---
+++ public/js/script.js
@@ -0,0 +1,35 @@
+// hamburger icon
+const hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", () => hamburger.classList.toggle("active"));
+
+// margin top because of fixed menu
+const margin = document.querySelector("#js-navbar").offsetHeight;
+document.querySelector("#router-view").style.marginTop = margin + "px";
+
+// handling modal and carousel to work ok
+function handleModal(index) {
+    const modal = new bootstrap.Modal(document.querySelector("#modal-"+index));
+    const carousel = document.querySelector("#inner-"+index);
+
+    modal.show();
+    carousel.children[0].classList.add("active");
+    document.querySelector("#modal-"+index).addEventListener('hide.bs.modal', () => {
+        for (let index = 0; index < carousel.children.length; index++)
+            carousel.children[index].classList.remove("active");
+    });
+}
+
+// hide menu on click
+const navMain = $(".navbar-collapse");
+navMain.on("click", ".js-hide", null, () => {
+    navMain.collapse('hide');
+    $(".hamburger").removeClass("active");
+});
+
+// hide menu on scroll
+window.onscroll = () => {
+    if (window.pageYOffset < 56)
+        document.getElementById("js-navbar").style.top = "0";
+    else
+        document.getElementById("js-navbar").style.top = "-80px";
+}
